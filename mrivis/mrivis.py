6ac39a63f63d9caad729afea92ecef3229dfde2e
Pradeep Reddy Raamana <praamana@research.baycrest.org>
2017-12-06 15:24:04 -0500

    major refactor allowing for a generic mixing of two images

--- mrivis/mrivis.py
+++ mrivis/mrivis.py
@@ -1,5 +1,4 @@
-
-__all__ = ['checkerboard']
+__all__ = ['checkerboard', 'red_green', 'voxelwise_diff']
 
 import numpy as np
 from matplotlib import pyplot as plt
@@ -7,16 +6,110 @@ import nibabel as nib
 from genericpath import exists as pexists
 from os.path import realpath
 
+
 def checkerboard(img_spec1=None,
                  img_spec2=None,
                  patch_size=10,
-                 num_rows = 2,
-                 num_cols = 6,
-                 rgb_mix=False,
+                 num_rows=2,
+                 num_cols=6,
                  rescale_intensity_range=None,
                  annot=None,
                  padding=5,
-                 output_path=None):
+                 output_path=None,
+                 figsize=None,):
+    "Checkerboard mixer"
+
+    mixer_params = dict(patch_size=patch_size)
+
+    fig = _compare(img_spec1,
+                   img_spec2,
+                   num_rows=num_rows,
+                   num_cols=num_cols,
+                   mixer='checker_board',
+                   rescale_intensity_range=rescale_intensity_range,
+                   annot=annot,
+                   padding=padding,
+                   output_path=output_path,
+                   figsize=None,
+                   **mixer_params)
+
+    return fig
+
+
+def red_green(img_spec1=None,
+              img_spec2=None,
+              alpha_channels=None,
+              num_rows=2,
+              num_cols=6,
+              rescale_intensity_range=None,
+              annot=None,
+              padding=5,
+              output_path=None,
+              figsize=None,):
+    "Red-green mixer, where the images become the red and green channels for the image."
+
+    if alpha_channels is None:
+        alpha_channels = [1, 1]
+
+    if not len(alpha_channels) == 2:
+        # not sure if we should make them sum to 1:  or not np.isclose(sum(alpha_channels), 1.0)
+        raise ValueError('Alpha must be two elements')
+
+    mixer_params = dict(alpha_channels=alpha_channels)
+    fig = _compare(img_spec1,
+                   img_spec2,
+                   num_rows=num_rows,
+                   num_cols=num_cols,
+                   mixer='red_green',
+                   annot=annot,
+                   padding=padding,
+                   output_path=output_path,
+                   figsize=None,
+                   **mixer_params)
+
+    return fig
+
+def voxelwise_diff(img_spec1=None,
+              img_spec2=None,
+              abs_value=True,
+              num_rows=2,
+              num_cols=6,
+              rescale_intensity_range=None,
+              annot=None,
+              padding=5,
+              output_path=None,
+                   figsize=None):
+    "Voxel-wise difference map."
+
+    if not isinstance(abs_value, bool):
+        abs_value = bool(abs_value)
+
+    mixer_params = dict(abs_value=abs_value)
+    fig = _compare(img_spec1,
+                   img_spec2,
+                   num_rows=num_rows,
+                   num_cols=num_cols,
+                   mixer='voxelwise_diff',
+                   annot=annot,
+                   padding=padding,
+                   output_path=output_path,
+                   figsize=None,
+                   **mixer_params)
+
+    return fig
+
+
+def _compare(img_spec1,
+             img_spec2,
+             num_rows=2,
+             num_cols=6,
+             mixer='checker_board',
+             rescale_intensity_range=None,
+             annot=None,
+             padding=5,
+             output_path=None,
+             figsize=None,
+             **kwargs):
     """
     Produces checkerboard comparison plot of two 3D images.
 
@@ -37,6 +130,10 @@ def checkerboard(img_spec1=None,
     num_cols : int
         number of panels (left to right) per row of each dimension.
 
+    mixer : str
+        type of mixer to produce the comparison figure.
+        Options: checker_board, red_green, diff_abs,
+
     rescale_intensity_range : bool or list
         range to rescale the intensity values to
 
@@ -46,18 +143,22 @@ def checkerboard(img_spec1=None,
     padding : int
         number of voxels to pad around each panel.
 
+    kwargs : dict
+        Additional arguments specific to the particular mixer
+        e.g. alpha_channels = [1, 1] for the red_green mixer
+
     Returns
     -------
 
     """
 
-    patch_size, num_rows, num_cols, padding = check_params(patch_size, num_rows, num_cols, padding)
+    num_rows, num_cols, padding = check_params(num_rows, num_cols, padding)
     img1, img2 = check_images(img_spec1, img_spec2)
     img1, img2 = crop_to_extents(img1, img2, padding)
 
-    num_panels = num_rows*num_cols*3
+    num_panels = num_rows * num_cols * 3
     # skipping few at first and last
-    skip_count = num_rows*num_cols
+    skip_count = num_rows * num_cols
 
     slices = list()
     for dim_size in img1.shape:
@@ -78,20 +179,22 @@ def checkerboard(img_spec1=None,
 
     plt.style.use('dark_background')
 
-    alpha_channels = [1, 1]
-
     num_axes = 3
-    fig, ax = plt.subplots(num_axes*num_rows, num_cols, figsize=[15, 15])
+    if figsize is None:
+        figsize = [15, 15]
+    fig, ax = plt.subplots(num_axes * num_rows, num_cols, figsize=figsize)
 
     # displaying some annotation text if provided
     # good choice would be the location of the input image (for future refwhen image is shared or misplaced!)
     if annot is not None:
         fig.suptitle(annot, backgroundcolor='black', color='g')
 
+    display_params = dict(interpolation='none', aspect='equal', origin='lower')
+
     ax = ax.flatten()
     ax_counter = 0
     for dim_index in range(3):
-        slices_this_dim = slices[dim_index][skip_count : -skip_count]
+        slices_this_dim = slices[dim_index][skip_count: -skip_count]
 
         for slice_num in slices_this_dim:
             plt.sca(ax[ax_counter])
@@ -100,26 +203,16 @@ def checkerboard(img_spec1=None,
             slice1 = get_axis(img1, dim_index, slice_num)
             slice2 = get_axis(img2, dim_index, slice_num)
 
-            if rgb_mix:
-                mixed = mix_color(slice1, slice2, alpha_channels)
-                cmap = 'gray'
-            else:
-                checkers = get_checkers(slice1.shape, patch_size)
-                mixed = mix_slices(slice1, slice2, checkers)
-                cmap = 'gray'
+            mixed, mixer_spec_params = _generic_mixer(slice1, slice2, mixer, **kwargs)
+            display_params.update(mixer_spec_params)
 
             if RescaleImages:
-                plt.imshow(mixed, imlim=img_intensity_range,
-                           interpolation = 'none', cmap=cmap,
-                           aspect='equal', origin='lower')
+                plt.imshow(mixed, imlim=img_intensity_range, **display_params)
             else:
-                plt.imshow(mixed,
-                           interpolation = 'none', cmap=cmap,
-                           aspect='equal', origin='lower')
+                plt.imshow(mixed, **display_params)
 
             # adjustments for proper presentation
             plt.axis('off')
-
 
     fig.tight_layout()
 
@@ -132,40 +225,79 @@ def checkerboard(img_spec1=None,
     return fig
 
 
+def _generic_mixer(slice1, slice2, mixer_name, **kwargs):
+    "Generic mixer to process two slices with appropriate mixer and return the composite to be displayed."
+
+    mixer_name = mixer_name.lower()
+    if mixer_name in ['red_green', 'rgb']:
+        mixed = _mix_color(slice1, slice2, **kwargs)
+        cmap = 'gray'
+    elif mixer_name in ['checkerboard', 'checker', 'cb', 'checker_board']:
+        checkers = _get_checkers(slice1.shape, **kwargs)
+        mixed = _mix_slices_in_checkers(slice1, slice2, checkers)
+        cmap = 'gray'
+    elif mixer_name in ['diff', 'voxelwise_diff', 'vdiff']:
+        mixed = _diff_image(slice1, slice2, **kwargs)
+        cmap = 'gray'
+    else:
+        raise ValueError('Invalid mixer name chosen.')
+
+    disp_params = dict(cmap=cmap)
+
+    return mixed, disp_params
+
+
+def _diff_image(slice1, slice2, abs_value=True):
+    "Computes the difference image"
+
+    diff = slice1-slice2
+
+    if abs_value:
+        diff = np.abs(diff)
+
+    return diff
+
+
 def get_axis(array, axis, slice_num):
     "Returns a fixed axis"
 
     slice_list = [slice(None)] * array.ndim
     slice_list[axis] = slice_num
-    slice_data = array[slice_list].T # transpose for proper orientation
+    slice_data = array[slice_list].T  # transpose for proper orientation
 
     return slice_data
 
 
 def check_int(num, num_descr):
+    "Validation and typecasting."
 
-    if not np.isfinite(num) or num<1:
+    if not np.isfinite(num) or num < 1:
         raise ValueError('{} is not finite or is not > 0'.format(num_descr))
 
     return int(num)
 
 
-def check_params(patch_size, num_rows, num_cols, padding):
+def check_patch_size(patch_size):
     "Validation and typcasting"
 
     patch_size = np.array(patch_size)
     if patch_size.size == 1:
-        patch_size = np.repeat(patch_size, 2)
+        patch_size = np.repeat(patch_size, 2).astype('int16')
+
+    return patch_size
+
+
+def check_params(num_rows, num_cols, padding):
+    "Validation and typcasting"
 
     num_rows = check_int(num_rows, 'num_rows')
     num_cols = check_int(num_cols, 'num_cols')
     padding = check_int(padding, 'padding')
 
-    return patch_size, num_rows, num_cols, padding
+    return num_rows, num_cols, padding
 
 
 def check_images(img_spec1, img_spec2):
-
     img1 = read_image(img_spec1)
     img2 = read_image(img_spec2)
 
@@ -205,13 +337,15 @@ def read_image(img_spec):
 
     # setting small stray values to zero
     stray_value = np.percentile(img.flatten(), 5)
-    img[img<stray_value] = 0.0
+    img[img < stray_value] = 0.0
 
     return img
 
 
-def get_checkers(slice_shape, patch_size):
+def _get_checkers(slice_shape, patch_size):
     " Creates checkerboard of a given tile size, filling a given slice."
+
+    patch_size = check_patch_size(patch_size)
 
     black = np.zeros(patch_size)
     white = np.ones(patch_size)
@@ -219,7 +353,7 @@ def get_checkers(slice_shape, patch_size):
 
     # using ceil so we can clip the extra portions
     num_tiles = np.ceil(np.divide(slice_shape, tile.shape)).astype(int)
-    checkers  = np.tile(tile, num_tiles)
+    checkers = np.tile(tile, num_tiles)
 
     # clipping any extra columns or rows
     if any(np.greater(checkers.shape, slice_shape)):
@@ -243,7 +377,7 @@ def scale_0to1(slice1, slice2):
     return slice1, slice2
 
 
-def mix_color(slice1, slice2, alpha_channels):
+def _mix_color(slice1, slice2, alpha_channels):
     "Mixing them as red and green channels"
 
     if slice1.shape != slice2.shape:
@@ -256,14 +390,14 @@ def mix_color(slice1, slice2, alpha_channels):
     slice1, slice2 = scale_0to1(slice1, slice2)
 
     mixed = np.zeros((slice1.shape[0], slice1.shape[1], 3))
-    mixed[:, :, 0] = alpha_channels[0]*slice1
-    mixed[:, :, 1] = alpha_channels[1]*slice2
-    mixed[:, :, 2] = 0.0 # leaving blue as zero
+    mixed[:, :, 0] = alpha_channels[0] * slice1
+    mixed[:, :, 1] = alpha_channels[1] * slice2
+    mixed[:, :, 2] = 0.0  # leaving blue as zero
 
     return mixed
 
 
-def mix_slices(slice1, slice2, checkers):
+def _mix_slices_in_checkers(slice1, slice2, checkers):
     "Mixes the two slices in alternating areas specified by checkers"
 
     if slice1.shape != slice2.shape or slice2.shape != checkers.shape:
@@ -301,14 +435,13 @@ def crop_coords(img, padding):
     else:
         min_coords = np.array([arr.min() for arr in coords])
         max_coords = np.array([arr.max() for arr in coords])
-        beg_coords = np.fmax(0        , min_coords - padding)
+        beg_coords = np.fmax(0, min_coords - padding)
         end_coords = np.fmin(img.shape, max_coords + padding)
 
     return beg_coords, end_coords
 
 
 def crop_3dimage(img, beg_coords, end_coords):
-
     cropped_img = img[
                   beg_coords[0]:end_coords[0],
                   beg_coords[1]:end_coords[1],
@@ -319,7 +452,6 @@ def crop_3dimage(img, beg_coords, end_coords):
 
 
 def cli_run():
-
     raise NotImplementedError
 
 
