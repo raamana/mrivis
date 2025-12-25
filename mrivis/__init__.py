d60f838e47271ad9c2acc721c2349ea7a2302278
Pradeep Reddy Raamana <raamana@gmail.com>
2025-12-25 11:29:38 -0500

    Remove Python 2 compatibility code
    - Remove version_info checks and conditional imports
    - Remove encoding declaration (not needed in Python 3)

--- mrivis/__init__.py
+++ mrivis/__init__.py
@@ -1,5 +1,3 @@
-# -*- coding: utf-8 -*-
-
 """Top-level package for mrivis."""
 
 __author__ = """Pradeep Reddy Raamana"""
@@ -9,21 +7,12 @@ __all__ = ['checkerboard', 'color_mix', 'voxelwise_diff',
            'Collage', 'SlicePicker', 'Carpet', 'MiddleSlicePicker', 'MidCollage',
            'aseg_on_mri', 'color_maps']
 
-from sys import version_info
-
-if version_info.major > 2:
-    from mrivis.workflow import (checkerboard, color_mix, voxelwise_diff,
-                                 aseg_on_mri)
-    from mrivis.base import (Collage, MidCollage,
-                             SlicePicker, MiddleSlicePicker,
-                             Carpet)
-    from mrivis import color_maps
-else:
-    # from .mrivis import checkerboard
-    raise NotImplementedError('mrivis requires Python 3 or higher. Please upgrade.')
-
-
-del version_info
+from mrivis.workflow import (checkerboard, color_mix, voxelwise_diff,
+                             aseg_on_mri)
+from mrivis.base import (Collage, MidCollage,
+                         SlicePicker, MiddleSlicePicker,
+                         Carpet)
+from mrivis import color_maps
 
 try:
     from ._version import __version__
